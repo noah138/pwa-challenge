41cfc68444d1380a991db957e68e8a3d47b4a94c
noah138 <noahschwartz@live.com>
2022-11-30 01:17:44 -0800

    heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,8 +31,8 @@ module.exports = () => {
         description: 'Takes notes with JavaScript syntax highlighting!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
